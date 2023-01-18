4378638b8f5f1880ff04823a919d4087db06137b
shvam0000 <shvam0000@gmail.com>
2023-01-18 18:10:48 +0530

    📱 fix responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Home } from './pages/';
 
 const App = () => {
   return (
-    <div className="font-Nunito min-h-screen bg-[#585CBA] p-20 text-white">
+    <div className="font-Nunito min-h-screen min-w-screen bg-[#585CBA] sm:p-10 md:p-20 text-white">
       <Home />
     </div>
   );
