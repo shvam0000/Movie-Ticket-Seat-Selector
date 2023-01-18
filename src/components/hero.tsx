4aca8eb53c52592fd0a0fc8cb2d4a2f4b245bd62
shvam0000 <shvam0000@gmail.com>
2023-01-18 18:04:10 +0530

    💄 UI Changes

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,11 +4,11 @@ const Hero = () => {
       <h1 className="text-4xl">Choose seats</h1>
       <div className="flex mt-2 mb-6">
         <div className="flex items-center pr-2">
-          <div className="bg-yellow-500 rounded-full h-5 w-5"></div>
+          <div className="bg-gray-300 rounded-full h-5 w-5"></div>
           <div className="text-lg px-2 text-gray-300">Available</div>
         </div>
         <div className="flex items-center px-2">
-          <div className="bg-gray-100 rounded-full h-5 w-5"></div>
+          <div className="bg-yellow-500 rounded-full h-5 w-5"></div>
           <div className="text-lg px-2 text-gray-300">Reserved</div>
         </div>
         <div className="flex items-center px-2">
